0b774a15c577d3f22b24731be988a527d66e1c2b
Andrei Maris <xarenwo@gmail.com>
2017-08-29 23:17:12 +0200

    Update

--- app/app.jsx
+++ app/app.jsx
@@ -1,18 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var objOne = {
-    name: 'Andrew',
-    location: 'Milan'
-};
 
-var objTwo = {
-    age:24,
-    ...objOne
 
-};
-
-console.log(objTwo);
 
 ReactDOM.render(
   <h1>React JS Boilerplate</h1>,
